41703fe93a4d8c2d9a5391336065be2a227a3f46
prashanth38 <32742210+prashanth38@users.noreply.github.com>
2020-09-03 14:53:31 +0530

    Add files via upload

---
+++ environments/environment.ts
@@ -0,0 +1,15 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB9TNl-fviHC5ASeA7uzmeu8IrNAGRSA8U",
+    authDomain: "final-57b26.firebaseapp.com",
+    databaseURL: "https://final-57b26.firebaseio.com",
+    projectId: "final-57b26",
+    storageBucket: "final-57b26.appspot.com",
+    messagingSenderId: "1045783386268"
+  }
+};
